b060f4f8d9162c52039aebcafe38e23a3f5e92f8
thegwanj <93459099+thegwanj@users.noreply.github.com>
2025-02-06 20:49:12 -0800

    Finished item 3 of the midterm

--- test/test.js
+++ test/test.js
@@ -23,3 +23,18 @@ const mockReviews = [
       "The coffee was great but the employees didn't let me stay past closing! ): Worst experience ever.",
   },
 ];
+
+// Test suite for testing the calculateStarAverage function
+describe("Tests calculateStarAverage function", () => {
+
+  it("Should return an average star rating of 3", () => {
+    const results = calculateStarAverage(mockReviews);
+    assert.equal(results, 3);
+  });
+  it("Should return an average star rating of 3.5 after getting a 5-star review", () => {
+    mockReviews.push({username: "tester", image: "./images/avatar2.png", star: 5, review: "Very cool"})
+
+    const results = calculateStarAverage(mockReviews);
+    assert.equal(results, 3.5);
+  });
+});
